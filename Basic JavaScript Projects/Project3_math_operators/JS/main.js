7dc6e8a0d098ddfbe286a004ddf67ddca71b7252
4myNicole <91946031+4myNicole@users.noreply.github.com>
2022-01-19 14:55:23 +0000

    Add files via upload

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,45 @@
+function addition_Function () {
+    var addition =  2 + 2;
+    document.getElementById("math").innerHTML =  addition; 
+}
+
+function subtract_function() {
+             var subtract = 5-2;
+             document.getElementByID("math").innerHTML =  subtract;
+}
+
+function multiply_operator() {
+            var multiply = 30 * 5;
+            document.getElementByID("math").innerHTML =  multiply;
+}
+
+function modulus_operator() {
+            var simple_math = 25 % 6;
+            document.getElementByID("math").innerHTML =  simple_math;
+}
+
+function negation_operator() {
+            var x = 10;
+            document.getElementById("math").innerHTML = -x;
+}
+
+function increment_operator() {
+var X = 20;
+X++;
+document.write(x);
+}
+
+function decrement_operator() {
+var X = 69;
+X--;
+document.write(x);
+}
+
+
+window.alert(Math.random() * 1000000);
+
+
+function math_object() {
+    var x = math.PI;
+    document.getElementById("math").innerHTML = math.PI;
+}
