6a3ff1c44d4ef682ab1d5c39819687871e912c1b
4myNicole <91946031+4myNicole@users.noreply.github.com>
2022-02-16 10:44:19 +0000

    Update main.js

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,6 +1,4 @@
-
-
-    function type_of() {
+function type_of() {
          var type_of = (typeof "Word");
              document.getElementById("type.of").innerHTML = type_of;
     }
@@ -12,43 +10,43 @@
             document.getElementById("Infinity").innerHTML = large_number;
     }
 
-    function minus_infinity() {
+    function minus_infinity() { 
         var small_number = -3E310;
             document.getElementById("minus.Infinity").innerHTML = small_number;
         }
 
 
-    function boolean() {
+    function boolean() { //display false
         var boolean_logic = (3>8);
             document.getElementById("boolean").innerHTML = boolean_logic;
         }
 
-    function coercion() {
+    function coercion() { //display twenty5
         var type_coercion = ("twenty" + 5);
             document.getElementById("coercion").innerHTML = type_coercion;
             }
 
-    function double_equal() {
+    function double_equal() { //display true
          var double_equal = ("A" == "A");
             document.getElementById("double.equal").innerHTML = double_equal;
                 }
 
-    function triple_equal() {
+    function triple_equal() { //display false
          var triple_equal = ("Dog" === "Cat");
             document.getElementById("triple.equal").innerHTML = triple_equal;
                     }
 
 
-function double_and_tr() {
+function double_and_tr() { //display true
     var double_and_t = (5 > 2 && 10 > 4);
        document.getElementById("double.and.t").innerHTML = double_and_t;
                }
 
-function double_and_fal() {
+function double_and_fal() { //display false
     var double_and_f = (5 > 10 && 10 > 4);
         document.getElementById("double.and.f").innerHTML = double_and_f;
                            }
 
-        function not_Function() {
+        function not_Function() { //display false
              document.getElementById("Not").innerHTML = ! (20 > 10);
         }
